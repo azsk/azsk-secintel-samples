9e1149ff10902ce80837e5d4519a660cfc44165a
Vishal H <vihaibat@microsoft.com>
2019-09-16 23:05:17 +0530

    Added VSCode samples

---
+++ SecIntelSampleVSCode/AzureStorageSample.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Security.Cryptography;
+
+namespace SecIntelSample
+{
+    // Azure Storage Samples
+
+    public class AzureStorageSample
+    {
+
+        // Rule ID: azure_storate_sas_use_https
+
+        public static string GetAccountSASTokenErrorAboutInsecureProtocol()
+        {
+            const string ConnectionString =
+                "DefaultEndpointsProtocol=https;AccountName=account-name;AccountKey=account-key";
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConnectionString);
+
+            // Sample in which error is generated due to usage of insecure [HttpsOrHttp] protocol configuration
+
+            SharedAccessAccountPolicy policy = new SharedAccessAccountPolicy
+            {
+                Permissions =
+                    SharedAccessAccountPermissions.Read | SharedAccessAccountPermissions.Write |
+                    SharedAccessAccountPermissions.List,
+                Services = SharedAccessAccountServices.Blob | SharedAccessAccountServices.File,
+                ResourceTypes = SharedAccessAccountResourceTypes.Service,
+                SharedAccessExpiryTime = DateTime.UtcNow.AddHours(24),
+                Protocols = SharedAccessProtocol.HttpsOrHttp
+            };
+            return storageAccount.GetSharedAccessSignature(policy);
+        }
+
+
+        // Storage account Blob container creation samples
+        public static void CreateBlobContainer()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("");
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("mycontainer");
+            container.CreateIfNotExists();
+
+            // Sample in which warning is generated to be aware of container access policy
+
+            container.SetPermissions(new BlobContainerPermissions
+            {
+                PublicAccess = BlobContainerPublicAccessType.Blob
+            });
+
+            // Sample in which warning is generated to be aware of container access policy
+
+            container.SetPermissions(new BlobContainerPermissions
+            {
+                PublicAccess = BlobContainerPublicAccessType.Container
+            });
+
+            // Sample in which most restricted access type is used
+
+            container.SetPermissions(new BlobContainerPermissions
+            {
+                PublicAccess = BlobContainerPublicAccessType.Off
+            });
+        }
+
+        // Rule ID: azure_storage_blob_public_access
+        public static void CreateBlobContainerBlobAccessType()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("");
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("mycontainer");
+            container.CreateIfNotExists();
+
+            // Sample in which warning is generated to be aware of container access policy
+
+            container.SetPermissions(new BlobContainerPermissions
+            {
+                PublicAccess = BlobContainerPublicAccessType.Blob
+            });
+        }
+
+        // Rule ID: azure_storage_container_public_access 
+        public static void CreateBlobContainerPublicAccessType()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("");
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("mycontainer");
+            container.CreateIfNotExists();
+
+            // Sample in which warning is generated to be aware of container access policy
+
+            container.SetPermissions(new BlobContainerPermissions
+            {
+                PublicAccess = BlobContainerPublicAccessType.Container
+            });
+        }
+    }
+}
---
+++ SecIntelSampleVSCode/CryptoSample.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace SecIntelSample 
+{
+    public class CryptoSample
+    {
+        public static void RandomData()
+        {
+            // Insecure Random data generator
+            var random = new Random();
+ 
+            // Secure
+            var rng = new RNGCryptoServiceProvider();
+
+            // Insecure hashing algo
+            var md5 = new MD5CryptoServiceProvider();
+
+            // Insecure hashing algo
+            var sha1 = new SHA1CryptoServiceProvider();
+
+            // Secure
+            var sha256 = new SHA256CryptoServiceProvider();
+
+            //Insecure Encryption
+            var rijndael = new RijndaelManaged();
+
+            // Secure
+            var aes = new AesCryptoServiceProvider();
+
+            // crypto_enc_padding_ansix923
+            aes.Padding = PaddingMode.ANSIX923;
+
+            // crypto_enc_padding_iso10126
+            aes.Padding = PaddingMode.ISO10126;
+
+            // crypto_enc_padding_none
+            aes.Padding = PaddingMode.None;
+
+            // crypto_enc_padding_zeros
+            aes.Padding = PaddingMode.Zeros;
+
+            // safe
+
+            aes.Padding = PaddingMode.PKCS7;
+
+            // crypto_enc_ciphermode_ecb
+            aes.Mode = CipherMode.ECB;
+        }
+    }
+}
